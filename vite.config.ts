406ab9b6e186748b7d44f8a2db26b97f240afb62
sam-19 <26563023+sam-19@users.noreply.github.com>
2025-06-28 18:46:16 +0300

    Try copying docs assets
    Signed-off-by: sam-19 <26563023+sam-19@users.noreply.github.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
@@ -14,6 +15,19 @@ export default defineConfig({
                     })
                 }
             }
+        }),
+        viteStaticCopy({
+            structured: true,
+            targets: [
+                {
+                    dest: '/',
+                    rename: (_name, _ext, path) => {
+                        // Get the docs path from the full file path.
+                        return '../../../../' + path.substring(path.indexOf('/src/docs/') + 5)
+                    },
+                    src: 'src/docs/**/*.md',
+                },
+            ],
         }),
     ]
 })
